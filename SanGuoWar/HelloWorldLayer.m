befcb192cb8445e52294b30a481bf9f7d854ad58
Sheng Dong <sheng@fuzzproductions.com>
2013-07-24 00:16:15 -0400

    Rename ProjectName

---
+++ SanGuoWar/HelloWorldLayer.m
@@ -0,0 +1,135 @@
+//
+//  HelloWorldLayer.m
+//  Cocos2DSimpleGame
+//
+//  Created by Sheng Jun Dong on 7/21/13.
+//  Copyright Sheng 2013. All rights reserved.
+//
+
+
+// Import the interfaces
+#import "HelloWorldLayer.h"
+
+// Needed to obtain the Navigation Controller
+#import "AppDelegate.h"
+
+#pragma mark - HelloWorldLayer
+
+// HelloWorldLayer implementation
+@implementation HelloWorldLayer
+
+// Helper class method that creates a Scene with the HelloWorldLayer as the only child.
++(CCScene *) scene
+{
+	// 'scene' is an autorelease object.
+	CCScene *scene = [CCScene node];
+	
+	// 'layer' is an autorelease object.
+	HelloWorldLayer *layer = [HelloWorldLayer node];
+	
+	// add layer as a child to scene
+	[scene addChild: layer];
+
+	// return the scene
+	return scene;
+}
+
+// on "init" you need to initialize your instance
+-(id) init
+{
+	// always call "super" init
+	// Apple recommends to re-assign "self" with the "super's" return value
+	if(self=[super initWithColor:ccc4(255, 255, 255, 255)]){
+		CGSize size = [CCDirector sharedDirector].winSize;
+        CCSprite *player = [CCSprite spriteWithFile:@"player.png"];
+        player.position = ccp(player.contentSize.width/2, size.height/2);
+        [self addChild:player];
+		
+		// Default font size will be 28 points.
+		[CCMenuItemFont setFontSize:28];
+		
+
+	}
+    [self setTouchEnabled:YES];
+    [self schedule:@selector(gameLogic:) interval:1.0];
+	return self;
+}
+- (void)ccTouchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+    // Choose one of the touches to work with
+    UITouch *touch = [touches anyObject];
+    CGPoint location = [self convertTouchToNodeSpace:touch];
+    
+    // Set up initial location of projectile
+    CGSize winSize = [[CCDirector sharedDirector] winSize];
+    CCSprite *projectile = [CCSprite spriteWithFile:@"projectile.png"];
+    projectile.position = ccp(20, winSize.height/2);
+    
+    // Determine offset of location to projectile
+    CGPoint offset = ccpSub(location, projectile.position);
+    
+    // Bail out if you are shooting down or backwards
+    if (offset.x <= 0) return;
+    
+    // Ok to add now - we've double checked position
+    [self addChild:projectile];
+    
+    int realX = winSize.width + (projectile.contentSize.width/2);
+    float ratio = (float) offset.y / (float) offset.x;
+    int realY = (realX * ratio) + projectile.position.y;
+    CGPoint realDest = ccp(realX, realY);
+    
+    // Determine the length of how far you're shooting
+    int offRealX = realX - projectile.position.x;
+    int offRealY = realY - projectile.position.y;
+    float length = sqrtf((offRealX*offRealX)+(offRealY*offRealY));
+    float velocity = 480/1; // 480pixels/1sec
+    float realMoveDuration = length/velocity;
+    
+    // Move projectile to actual endpoint
+    [projectile runAction:
+     [CCSequence actions:
+      [CCMoveTo actionWithDuration:realMoveDuration position:realDest],
+      [CCCallBlockN actionWithBlock:^(CCNode *node) {
+         [node removeFromParentAndCleanup:YES];
+     }],
+      nil]];
+    
+}
+-(void)gameLogic:(ccTime)dt {
+    [self addMonster];
+}
+- (void) addMonster {
+    
+    CCSprite * monster = [CCSprite spriteWithFile:@"monster.png"];
+    
+    // Determine where to spawn the monster along the Y axis
+    CGSize winSize = [CCDirector sharedDirector].winSize;
+    int minY = monster.contentSize.height / 2;
+    int maxY = winSize.height - monster.contentSize.height/2;
+    int rangeY = maxY - minY;
+    int actualY = (arc4random() % rangeY) + minY;
+    
+    // Create the monster slightly off-screen along the right edge,
+    // and along a random position along the Y axis as calculated above
+    monster.position = ccp(winSize.width + monster.contentSize.width/2, actualY);
+    [self addChild:monster];
+    NSLog(@"Starting at: x: %f, y: %f", monster.position.x, monster.position.y);
+    
+    // Determine speed of the monster
+    int minDuration = 2.0;
+    int maxDuration = 4.0;
+    int rangeDuration = maxDuration - minDuration;
+    int actualDuration = (arc4random() % rangeDuration) + minDuration;
+    NSLog(@"duration: %d", actualDuration);
+    // Create the actions
+    CCMoveTo * actionMove = [CCMoveTo actionWithDuration:actualDuration
+                                                position:ccp(-monster.contentSize.width/2, actualY)];
+    CCCallBlockN * actionMoveDone = [CCCallBlockN actionWithBlock:^(CCNode *node) {
+        [node removeFromParentAndCleanup:YES];
+    }];
+    [monster runAction:[CCSequence actions:actionMove, actionMoveDone, nil]];
+    
+}
+
+@end
---
+++ SanGuoWar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Cocos2DSimpleGame
+//
+//  Created by Sheng Jun Dong on 7/21/13.
+//  Copyright Sheng 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+        return retVal;
+    }
+}
